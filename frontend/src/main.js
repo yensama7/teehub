53ad2255afda81e2b9a49123495d10592d39bffa
chiemerie uzonu <147042568+yensama7@users.noreply.github.com>
2024-11-10 18:04:18 +0100

    Update main.js
    test deployment

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -4,6 +4,6 @@ import router from './router'
 import store from './store'
 import axios from 'axios'
 
-axios.defaults.baseURL = 'http://127.0.0.1:8000'
+axios.defaults.baseURL = 'https://b28f-197-211-58-107.ngrok-free.app'
 
 createApp(App).use(store).use(router, axios).mount('#app')
